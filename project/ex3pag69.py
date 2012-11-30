c408260f0d9275bbc2a3874c33ba70109569d63e
alex90s <alex90_s@yahoo.com>
2012-11-30 11:11:49 +0100

    Ex3pag69

---
+++ project/ex3pag69.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 13, 2012
+
+@author: aLeX
+'''
+Mylist = ["lemon","apple","juice","orange"]
+if("lem" == Mylist[0]):
+    print("true")
+elif("apple" in Mylist[1]):
+    print(" 2nd element")
+else:
+    print(" the element is not ")
+
